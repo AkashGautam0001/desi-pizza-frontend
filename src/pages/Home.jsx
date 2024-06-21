4f1ecebbf73ce2dd79f4817345dca0d62016ad72
Akash Gautam <akashgautam01584@gmail.com>
2024-06-21 19:25:09 +0530

    folder name changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
 function Home() {
 	return (
 		<div>
-			<h1>Home</h1>
+			<section className="flx flex-col-reverse items-center justify-center py-5 md:flex-row md:gap-7 bg-gradient-to-r from-amber-300 to-purple-400">
+				<div className="w-4/6 ml-4 text-center md:w-2/6 md:text-left">
+					<div className="flex justify-center text-4xl md:justify-normal">
+						<h1 className="pb-5 font-bold text-transparent bg-gradient-to-r from-amber-300 to-purple-400 bg-clip-text">
+							Enjoy the slice{" "}
+						</h1>
+						<h1>🎃</h1>
+					</div>
+				</div>
+			</section>
 		</div>
 	);
 }
