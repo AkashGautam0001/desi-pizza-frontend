681dae0f392afaf9168aa801e9ab1288ac413d18
Akash Gautam <akashgautam01584@gmail.com>
2024-06-27 22:54:35 +0530

    added logout thunks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,16 @@
+import { useDispatch, useSelector } from "react-redux";
 import Pizzalogo from "../assets/images/pizza1.png";
+import { Link } from "react-router-dom";
+import { logout } from "../redux/slices/AuthSlice";
 
 function Navbar() {
+	const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+	const dispatch = useDispatch();
+
+	async function handleLogout(e) {
+		e.preventDefault();
+		dispatch(logout());
+	}
 	return (
 		<nav className="flex items-center justify-around h-16 text-[#6B72200] font-mono border-none shadow-md">
 			<div className="flex items-center justify-center">
@@ -21,6 +31,17 @@ function Navbar() {
 						<p>Services </p>
 					</li>
 					<li className="hover:text-[#FF9110]"> About </li>
+				</ul>
+			</div>
+			<div>
+				<ul>
+					<li className="hover:text-[#FF9110]">
+						{isLoggedIn ? (
+							<Link onClick={handleLogout}>Logout</Link>
+						) : (
+							<Link to={"/auth/login"}>Login</Link>
+						)}
+					</li>
 				</ul>
 			</div>
 		</nav>
--- src/redux/slices/AuthSlice.js
+++ src/redux/slices/AuthSlice.js
@@ -31,11 +31,76 @@ export const createAccount = createAsyncThunk(
 	}
 );
 
+export const login = createAsyncThunk("/auth/login", async (data) => {
+	console.log("incoming data to thunk", data);
+	try {
+		const response = axiosInstance.post("/auth/login", data);
+		toast.promise(response, {
+			success: (resolvedPromise) => {
+				return resolvedPromise?.data?.message;
+			},
+			loading: "Hold back tight, we are registering your id...",
+			error: "Ohh No!, Something went wrong, Please try again.",
+		});
+
+		const apiResponse = await response;
+		console.log(apiResponse);
+		return apiResponse;
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+export const logout = createAsyncThunk("/auth/logout", async () => {
+	console.log("incoming data to thunk");
+	try {
+		const response = axiosInstance.post("/auth/logout");
+		toast.promise(response, {
+			success: (resolvedPromise) => {
+				return resolvedPromise?.data?.message;
+			},
+			loading: "Loading out...",
+			error: "Ohh No!, Something went wrong, Please try again.",
+		});
+
+		const apiResponse = await response;
+		console.log(apiResponse);
+		return apiResponse;
+	} catch (error) {
+		console.log(error);
+	}
+});
+
 const AuthSlice = createSlice({
 	name: "auth",
 	initialState,
 	reducers: {},
-	// extraReducers: {},
+	extraReducers: (builder) => {
+		builder
+			.addCase(login.fulfilled, (state, action) => {
+				//reducer which execute when the login thunk is fullfilled
+				state.isLoggedIn = true;
+				state.role = action?.payload?.data?.data?.userRole;
+				state.data = action?.payload?.data?.data?.userData;
+
+				localStorage.setItem("isLoggedIn", true);
+				localStorage.setItem(
+					"role",
+					action?.payload?.data?.data?.userRole
+				);
+				localStorage.setItem(
+					"data",
+					JSON.stringify(action?.payload?.data?.data?.userData)
+				);
+			})
+			.addCase(logout.fulfilled, (state, action) => {
+				localStorage.clear();
+
+				state.isLoggedIn = false;
+				state.role = "";
+				state.data = {};
+			});
+	},
 });
 
 export default AuthSlice.reducer;
